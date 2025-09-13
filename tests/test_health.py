0ebb1454733d08b1a2e98f01a35d0b8a1b0534d7
Pablo Alejandro D'ottavio Robledo <pabloalejandrod@gmail.com>
2025-09-13 11:40:32 -0300

    removed files to move to pw

--- tests/test_health.py
+++
@@ -1,6 +0,0 @@
-# tests/test_health.py
-def test_root_is_up_json(http, base_url):
-    resp = http.get(f"{base_url}/", timeout=3)
-    assert resp.status_code == 200
-    assert resp.headers.get("content-type", "").lower().startswith("application/json")
-    assert resp.json() == {"status": "Stock API up!!!!!"}
